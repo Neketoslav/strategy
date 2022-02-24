334a3584c5dcaa3b9ca77f7e9a36fcb7a8d917b2
Nikita <stalker6661666@gmail.com>
2022-02-24 13:34:34 +0300

    4

---
+++ Assets/Scripts/Core/AttackCommandCommandCreator.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading;
+using Zenject;
+
+public class AttackCommandCommandCreator : CancellableCommandCreatorBase<IAttackCommand, IAttackable>
+{
+	protected override IAttackCommand createCommand(IAttackable argument) => new AttackCommand(argument);
+}
+
+
